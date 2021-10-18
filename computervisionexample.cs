5f56561ac7df601770d37de126728bd409acaf72
Christian Pedersen <cp.it@cbs.dk>
2021-10-18 09:22:20 +0200

    Your comment

--- computervisionexample.cs
+++ computervisionexample.cs
@@ -14,6 +14,9 @@ using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 using Newtonsoft.Json.Linq;
 using System.Threading;
 using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
 
 namespace My.Functions
 {
@@ -24,7 +27,7 @@ namespace My.Functions
         private const string ANALYZE_URL_IMAGE = "https://berlingske.bmcdn.dk/media/cache/resolve/image_x_large/image/131/1316138/23476506-health-coronavirusglobal-groceries.jpg";
 
         [FunctionName("computervisionexample")]
-        public static async Task<IActionResult> Run(
+        public static async Task<HttpResponseMessage> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
@@ -32,18 +35,36 @@ namespace My.Functions
 
             string imageurl = req.Query["imageurl"];
 
-            string result = "";
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            imageurl = imageurl ?? data?.imageurl;
+
+            log.LogInformation("imageurl" + imageurl);
+            List<string> result = new List<string>();
             if (imageurl != "" && imageurl != null)
             {
-                ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
-                result = await AnalyzeImageUrl(client, imageurl);
+                if (CheckURLValid(imageurl))
+                {
+                    ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+                    result = await AnalyzeImageUrl(client, imageurl, log);
+                }
+                else
+                {
+                    result.Add("Url is invalid.");
+                }
             }
             else
             {
-                result = "invalid image";
+                result.Add("Parameter not found");
             }
 
-            return new OkObjectResult(result);
+            var myObj = new { response= result};
+            var jsonToReturn = JsonConvert.SerializeObject(myObj);
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
+            };
         }
 
         public static ComputerVisionClient Authenticate(string endpoint, string key)
@@ -54,14 +75,14 @@ namespace My.Functions
             return client;
         }
 
-        public static async Task<string> AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        public static bool CheckURLValid(this string source) => Uri.TryCreate(source, UriKind.Absolute, out Uri uriResult) && uriResult.Scheme == Uri.UriSchemeHttps;
+
+        public static async Task<List<string>> AnalyzeImageUrl(ComputerVisionClient client, string imageUrl, ILogger log)
         {
-            Console.WriteLine("----------------------------------------------------------");
-            Console.WriteLine("ANALYZE IMAGE - URL");
-            Console.WriteLine();
-
-
-            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            List<string> result = new List<string>();
+            try
+            {
+                List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
             {
                 VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
                 VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
@@ -70,16 +91,45 @@ namespace My.Functions
                 VisualFeatureTypes.Objects
             };
 
-            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, visualFeatures: features);
+                ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, visualFeatures: features);
 
-            string result = "";
-            Console.WriteLine("Summary:");
-            foreach (var caption in results.Description.Captions)
-            {
-                result += $"{Environment.NewLine}{caption.Text} with confidence {caption.Confidence}";
-                Console.WriteLine($"{caption.Text} with confidence {caption.Confidence}");
+
+                 log.LogInformation("Summary:");
+                foreach (var caption in results.Description.Captions)
+                {
+                    result.Add($"{caption.Text} with confidence {caption.Confidence}");
+                     log.LogInformation($"{caption.Text} with confidence {caption.Confidence}");
+                }
+
+                // Well-known (or custom, if set) brands.
+                log.LogInformation("Brands:");
+                foreach (var brand in results.Brands)
+                {
+                    result.Add($"Logo of {brand.Name} with confidence {brand.Confidence} at location {brand.Rectangle.X}, " +
+                      $"{brand.Rectangle.X + brand.Rectangle.W}, {brand.Rectangle.Y}, {brand.Rectangle.Y + brand.Rectangle.H}");
+
+                     log.LogInformation($"Logo of {brand.Name} with confidence {brand.Confidence} at location {brand.Rectangle.X}, " +
+                      $"{brand.Rectangle.X + brand.Rectangle.W}, {brand.Rectangle.Y}, {brand.Rectangle.Y + brand.Rectangle.H}");
+                }
+
+                // Faces
+                log.LogInformation("Faces:");
+                foreach (var face in results.Faces)
+                {
+                    result.Add($"A {face.Gender} of age {face.Age} at location {face.FaceRectangle.Left}, " +
+                      $"{face.FaceRectangle.Left}, {face.FaceRectangle.Top + face.FaceRectangle.Width}, " +
+                      $"{face.FaceRectangle.Top + face.FaceRectangle.Height}");
+                    log.LogInformation($"A {face.Gender} of age {face.Age} at location {face.FaceRectangle.Left}, " +
+                      $"{face.FaceRectangle.Left}, {face.FaceRectangle.Top + face.FaceRectangle.Width}, " +
+                      $"{face.FaceRectangle.Top + face.FaceRectangle.Height}");
+                }
+
             }
-
+            catch (Exception e)
+            {
+                 log.LogInformation("Exception: " + e.Message);
+                result.Add("Could not load image.");
+            }
             return result;
         }
     }
